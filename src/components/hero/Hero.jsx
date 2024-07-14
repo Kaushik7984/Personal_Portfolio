00be7be9379ba7f1efafa66e9166c7a27f7ca039
Kaushik7984 <kaushik7984@gmail.com>
2024-07-15 01:53:57 +0530

    modified component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,23 +1,64 @@
+import { motion } from 'framer-motion'
 import './hero.scss'
+
+const textVariants = {
+    initial: {
+        x: -500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+    scrollButton:{
+        opacity:0,
+        y:10,
+        transition:{
+            duration:2,
+            repeat:Infinity
+        }
+    },
+}
+const sliderVariants = {
+    initial: {
+        x: 0,
+    },
+    animate: {
+        x: "-220%",
+        opacity: 1,
+        transition: {
+            repeat:Infinity,
+            repeatType:"mirror",
+            duration: 20,
+        },
+    },
+}
 
 const Hero = () => {
     return (
         <div className='hero'>
             <div className="wrapper">
-                <div className="textContainer">
-                    <h2>Kaushik Tapaniya</h2>
-                    <h1>Frontend Developer</h1>
-                    <div className="buttons">
-                        <button>See the Latest Works</button>
-                        <button>Contact Me</button>
-                    </div>
-                    <img src="/scroll.png" alt="scroll" />
+                <motion.div className="textContainer" variants={textVariants} initial='initial' animate='animate'>
+                    <motion.h2 variants={textVariants}>Kaushik Tapaniya</motion.h2>
+                    <motion.h1 variants={textVariants}>Frontend Developer</motion.h1>
+                    <motion.div className="buttons" variants={textVariants}>
+                        <button variants={textVariants}>See the Latest Works</button>
+                        <button variants={textVariants}>Contact Me</button>
+                    </motion.div>
+                    <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="scroll" />
+                </motion.div>
+                </div >
+                <motion.div variants={sliderVariants}  initial='initial' animate='animate' className="slidingTextContainer">
+                    Javascript ReactJs
+                </motion.div>
+                <div className="imageContainer">
+                    <img src=".images/photo.png" alt="" />
                 </div>
-            </div>
-            <div className="imageContainer">
-                <img src=".images/photo.png" alt="" />
-            </div>
-        </div>
+            </div >
     )
 }
 
