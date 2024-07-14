952afe20dda974148cf5984660857c9f54c5be4c
Kaushik7984 <kaushik7984@gmail.com>
2024-07-14 23:32:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './app.scss'
+import Navbar from './components/navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <section><Navbar /></section>
+      <section>home</section>
+      <section>home</section>
+      <section>home</section>
+      <section>home</section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/sidebar/links/Links.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Links = () => {
+
+  const items = ["Home", "About", "Projects"]
+  return (
+    <div>Links</div>
+  )
+}
+
+export default Links
